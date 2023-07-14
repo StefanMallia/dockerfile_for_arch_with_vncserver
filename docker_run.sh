db44d54442dbb39701c0ce244188356a342be5c6
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2023-07-14 22:32:26 +0200

    exec with arch user

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 docker stop arch-vnc-container && docker rm arch-vnc-container
 docker run -td -p 5901:5901 --name arch-vnc-container arch-linux-vnc
-#docker exec -it arch-vnc-container /bin/bash
+docker exec --user=arch -it arch-vnc-container /bin/bash
 
