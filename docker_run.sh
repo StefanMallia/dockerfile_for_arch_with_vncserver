d5512fe50a9045a18bf5a83e260d11e91e9a3944
StefanMallia <5004438+StefanMallia@users.noreply.github.com>
2024-03-18 22:45:47 +0100

    Container name as argument

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,16 @@
-docker stop arch-vnc-container && docker rm arch-vnc-container
-docker run -td -p 5901:5901 --name arch-vnc-container arch-linux-vnc
-docker exec --user=arch --workdir=/home/arch -it arch-vnc-container /bin/bash
+if [ -z "$1" ]; then
+    echo "Provide container name as argument"
+    exit 1
+fi
+
+docker stop $1 && docker rm $1
+docker run -td -p 5901:5901 --name $1 arch-linux-vnc\
+    || \
+    (\
+        echo "Assigning random port."\
+        && docker rm $1\
+        && docker run -td -p :5901 --name $1 arch-linux-vnc\
+        && docker ps -f name=$1\
+    )
+docker exec --user=arch --workdir=/home/arch -it $1 /bin/bash
 
